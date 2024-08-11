fc2f956f1b0276e3486b01fa0fa00668008ed34e
Zackary Liel <47030040+zliel@users.noreply.github.com>
2024-08-10 20:13:39 -0700

    Updated the colors for the text of the about page

--- src/Routes/About.js
+++ src/Routes/About.js
@@ -1,30 +1,38 @@
 import React from 'react';
 import {Grid, List, ListItem, ListItemText, Typography} from "@mui/material";
+import {useTheme} from "@mui/material/styles";
 
 function About() {
+    const theme = useTheme();
+    const primaryHeaderColor = theme.palette.mode === "light" ? theme.palette.primary.main : theme.palette.primary.dark
+    const secondaryHeaderColor = theme.palette.mode === "light" ? theme.palette.secondary.main : theme.palette.secondary.dark
     return (
-        <Grid container alignSelf={"center"} justify={"center"} align={"center"} gap={2} direction={"column"} width={"80%"}>
+        <Grid container alignSelf={"center"} justify={"center"} align={"center"} gap={2} direction={"column"}
+              width={"80%"}>
             <Grid item sm>
-                <Typography variant={"h4"} color={"primary"}>
+                <Typography variant={"h4"} color={primaryHeaderColor}>
                     About LinguaTile
                 </Typography>
             </Grid>
 
             <Grid item sm>
-                <Typography variant={"h5"} color={"primary"}>What is this?</Typography>
-                <Typography variant={"h6"} color={"secondary"}>This is a language learning platform I'm developing, aimed at English learners learning Japanese</Typography>
+                <Typography variant={"h5"} color={secondaryHeaderColor}>What is this?</Typography>
+                <Typography variant={"h6"}>This is a language learning platform I'm developing,
+                    aimed at English learners learning Japanese</Typography>
             </Grid>
 
             <Grid item sm>
-                <Typography variant={"h5"} color={"primary"}>Why am I making this?</Typography>
-                <Typography variant={"h6"} color={"secondary"}>I wanted to recreate and combine aspects of the various learning platforms I've used while learning Japanese.
-                    I also really wanted to practice building full-stack applications, and I figured that since I love education and learning, I may as well practice by making something related to it.
+                <Typography variant={"h5"} color={primaryHeaderColor}>Why am I making this?</Typography>
+                <Typography variant={"h6"}>I wanted to recreate and combine aspects of the various
+                    learning platforms I've used while learning Japanese.
+                    I also really wanted to practice building full-stack applications, and I figured that since I love
+                    education and learning, I may as well practice by making something related to it.
                 </Typography>
             </Grid>
 
             <Grid item sm>
-                <Typography variant={"h5"} color={"primary"}>What's the stack?</Typography>
-                <Typography variant={"h6"} color={"secondary"}>Front End</Typography>
+                <Typography variant={"h5"} color={secondaryHeaderColor}>What's the stack?</Typography>
+                <Typography variant={"h6"} color={primaryHeaderColor}>Front End</Typography>
                 <List>
                     <ListItem>
                         <ListItemText primary={"ReactJS"} sx={{textAlign: "center"}}/>
@@ -37,26 +45,28 @@ function About() {
                     </ListItem>
                 </List>
 
-                <Typography variant={"h6"} color={"secondary"}>Back End</Typography>
+                <Typography variant={"h6"} color={primaryHeaderColor}>Back End</Typography>
                 <List>
                     <ListItem>
                         <ListItemText primary={"FastAPI"} sx={{textAlign: "center"}}/>
                     </ListItem>
                     <ListItem>
-                        <ListItemText primary={"Redis"} secondary={"To store translations for optimization"} sx={{textAlign: "center"}}/>
+                        <ListItemText primary={"Redis"} secondary={"To store translations for optimization"}
+                                      sx={{textAlign: "center"}}/>
                     </ListItem>
                 </List>
             </Grid>
 
             <Grid item sm>
-                <Typography variant={"h5"} color={"primary"}>Why the green and purple color scheme?</Typography>
-                <Typography variant={"h6"} color={"secondary"}>My two favorite colors are green and purple, and I thought they looked nice together. I also wanted to practice using Material-UI's theming system.
+                <Typography variant={"h5"} color={primaryHeaderColor}>Why the green and purple color scheme?</Typography>
+                <Typography variant={"h6"}>My two favorite colors are green and purple, and I
+                    thought they looked nice together. I also wanted to practice using Material-UI's theming system.
                 </Typography>
             </Grid>
 
             <Grid item sm>
-                <Typography variant={"h5"} color={"primary"}>What do I have planned for the future?</Typography>
-                <Typography variant={"h6"} color={"secondary"}>I want to add a spaced repetition system, vocabulary
+                <Typography variant={"h5"} color={secondaryHeaderColor}>What do I have planned for the future?</Typography>
+                <Typography variant={"h6"}>I want to add a spaced repetition system, vocabulary
                     flashcards, and a guided sequence of Japanese grammar and vocabulary, mirroring what I've learned
                     and being updated as I learn more.
                 </Typography>
