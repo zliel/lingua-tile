69b3d59b79dcfcc4e9fad4381ac241cffd818e90
Zackary Liel <47030040+zliel@users.noreply.github.com>
2024-08-13 01:14:25 -0700

    Added validation for the password input

--- src/Routes/Signup.js
+++ src/Routes/Signup.js
@@ -12,10 +12,47 @@ function Signup() {
     const [open, setOpen] = React.useState(false)
     const [message, setMessage] = React.useState("")
 
-    const handleLogin = () => {
-        if (password !== confirmPassword) {
+
+    const isValidPassword = () => {
+        if (password.length < 8) {
+            setOpen(true)
+            setMessage("Password must be at least 8 characters long")
+            return false
+        } else if (password.length > 64) {
+            setOpen(true)
+            setMessage("Password must be less than or equal to 64 characters long")
+            return false
+        } else if (password !== confirmPassword) {
             setOpen(true)
             setMessage("Passwords do not match")
+            return false
+        } else if (!password.match(/^[a-zA-Z0-9!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]*$/)) {
+            setOpen(true)
+            setMessage("Password must only include letters, numbers, special characters, and spaces")
+            return false
+        } else if (!password.match(/[a-z]/)) {
+            setOpen(true)
+            setMessage("Password must include at least one lowercase letter")
+            return false
+        } else if (!password.match(/[A-Z]/)) {
+            setOpen(true)
+            setMessage("Password must include at least one uppercase letter")
+            return false
+        } else if (!password.match(/[0-9]/)) {
+            setOpen(true)
+            setMessage("Password must include at least one number")
+            return false
+        } else if (!password.match(/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/)) {
+            setOpen(true)
+            setMessage("Password must include at least one special character")
+            return false
+        }
+
+        return true
+    }
+
+    const handleLogin = () => {
+        if (!isValidPassword()) {
             return
         }
         axios.post("http://127.0.0.1:8000/api/users/signup", {username: username, password: password})
