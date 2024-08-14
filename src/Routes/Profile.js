b6bf2e721864858a1b40fa17274d1485921bfb5b
Zackary Liel <47030040+zliel@users.noreply.github.com>
2024-08-13 20:40:58 -0700

    Removed an unnecessary import

--- src/Routes/Profile.js
+++ src/Routes/Profile.js
@@ -1,4 +1,4 @@
-import React, {useContext, useState, useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import {Box, Typography, Button, Grid} from '@mui/material';
 import ConfirmationDialog from "../Components/ConfirmationDialog";
 import axios from 'axios';
