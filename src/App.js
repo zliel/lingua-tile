4cacb423e5158bd41f3334f81f881fa9d503b6d9
Zackary Liel <47030040+zliel@users.noreply.github.com>
2024-08-13 18:02:31 -0700

    Added the SnackbarProvider to the whole application

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import Login from "./Routes/Login";
 import Signup from "./Routes/Signup";
 import Profile from "./Routes/Profile";
 import UpdateProfile from "./Routes/UpdateProfile";
+import {SnackbarProvider} from "./Contexts/SnackbarContext";
 
 const purpleBase = "#8c00cc"
 const greenBase = "#18b201"
@@ -74,11 +75,13 @@ function App() {
 
     return (
         <ThemeProvider theme={theme}>
-            <CssBaseline/>
-            <BrowserRouter>
-                <NavBar onThemeSwitch={handleTheme}/>
-                <AppRoutes/>
-            </BrowserRouter>
+            <SnackbarProvider>
+                <CssBaseline/>
+                <BrowserRouter>
+                    <NavBar onThemeSwitch={handleTheme}/>
+                    <AppRoutes/>
+                </BrowserRouter>
+            </SnackbarProvider>
         </ThemeProvider>
     );
 }
