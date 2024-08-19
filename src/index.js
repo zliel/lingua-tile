31dd7fe94b8a27397d8801f90f999c7aec2448dd
Zackary Liel <47030040+zliel@users.noreply.github.com>
2024-08-19 16:13:37 -0700

    Added a QueryClient and Provider to the App

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {AuthProvider} from './Contexts/AuthContext';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const queryClient = new QueryClient();
 root.render(
-    <AuthProvider>
-        <App/>
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+            <App/>
+        </AuthProvider>
+    </QueryClientProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
