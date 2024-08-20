5b7255c22b26705b072a7f0c741ac7dcbe943aca
Zackary Liel <47030040+zliel@users.noreply.github.com>
2024-08-19 18:14:41 -0700

    Added a query for getting the user's information in UpdateProfile

--- src/Routes/UpdateProfile.js
+++ src/Routes/UpdateProfile.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import {Box, Typography, Button, Grid, TextField} from '@mui/material';
 import axios from 'axios';
+import {useQuery, useMutation} from '@tanstack/react-query'
 import {useNavigate} from 'react-router-dom';
 import {useSnackbar} from '../Contexts/SnackbarContext';
 import {useAuth} from '../Contexts/AuthContext';
@@ -8,33 +9,35 @@ import {useAuth} from '../Contexts/AuthContext';
 function UpdateProfile() {
     const {auth, logout} = useAuth();
     const [username, setUsername] = useState('');
-    const [user, setUser] = useState({});
     const navigate = useNavigate();
     const [password, setPassword] = useState("")
     const [confirmPassword, setConfirmPassword] = useState("")
     const {showSnackbar} = useSnackbar();
 
 
-    useEffect(() => {
-        // Fetch user data
-        const fetchUserData = async () => {
-            try {
-                const response = await axios.get(`http://127.0.0.1:8000/api/users`, {
-                    headers: {
-                        'Authorization': `Bearer ${auth.token}`
-                    }
-                });
-                setUsername(response.data.username);
-                setUser(response.data)
-            } catch (error) {
-                logout(() => navigate('/home'));
-            }
-        };
+    const { data: user, isError, isLoading } = useQuery({
+        queryKey: ['user', auth.token],
+        queryFn: async () => {
+            const response = await axios.get(`http://127.0.0.1:8000/api/users`, {
+                headers: {
+                    'Authorization': `Bearer ${auth.token}`
+                }
+            });
 
-        if (auth.isLoggedIn) {
-            fetchUserData();
+            setUsername(response.data.username);
+            return response.data;
+        },
+        onError: (error) => {
+            if (error.response.status === 401) {
+                showSnackbar("Session expired. Please log in again.", "error");
+                logout(() => navigate('/home'));
+            } else {
+                showSnackbar(`Error: ${error.response.data.detail}`, "error");
+            }
         }
     }, [auth, logout, navigate]);
+
+    });
 
     const isValidPassword = () => {
         const conditions = {
@@ -114,18 +117,18 @@ function UpdateProfile() {
     return (
         <Box sx={{display: 'flex', flexDirection: 'column', alignItems: 'center', mt: 4, justifyContent: 'center'}}>
             <Typography variant="h4" gutterBottom>Update Profile</Typography>
-            <Typography variant="h6" gutterBottom>Current Username: {username}</Typography>
+            <Typography variant="h6" gutterBottom>Current Username: {user.username}</Typography>
             <Grid container spacing={2} justifyContent="center" alignItems={"center"} direction={"column"}>
                 <Grid item>
-                    <TextField variant={"outlined"} label="New Username" value={user.username || ''}
-                               onChange={(e) => setUser({...user, username: e.target.value})}/>
+                    <TextField variant={"outlined"} label="New Username" value={username}
+                               onChange={(e) => setUsername(e.target.value)}/>
                 </Grid>
                 <Grid item>
-                    <TextField variant={"outlined"} label="New Password" value={password || ''}
+                    <TextField variant={"outlined"} label="New Password" value={password}
                                onChange={(e) => setPassword(e.target.value)}/>
                 </Grid>
                 <Grid item>
-                    <TextField variant={"outlined"} label="Confirm New Password" value={confirmPassword || ''}
+                    <TextField variant={"outlined"} label="Confirm New Password" value={confirmPassword}
                                onChange={(e) => setConfirmPassword(e.target.value)}/>
                 </Grid>
                 <Grid item>
