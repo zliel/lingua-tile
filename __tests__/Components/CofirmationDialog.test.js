ee14b8535209bc97a6d3fb96529e910b82658d61
Zackary Liel <47030040+zliel@users.noreply.github.com>
2024-09-30 12:56:24 -0700

    Moved the component tests to a component subdirectory

--- __tests__/Components/CofirmationDialog.test.js
+++ __tests__/Components/CofirmationDialog.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
-import ConfirmationDialog from "../src/Components/ConfirmationDialog";
+import ConfirmationDialog from "../../src/Components/ConfirmationDialog";
 
 describe("ConfirmationDialog", () => {
   it("renders with title and message", () => {
