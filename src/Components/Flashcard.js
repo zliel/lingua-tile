e3dedcb11713712840bf2acc90f6813fac4dbcad
Zackary Liel <47030040+zliel@users.noreply.github.com>
2024-10-10 20:45:43 -0700

    Updated the button sizes and added a slide/fade in/out animation to cards

--- src/Components/Flashcard.js
+++ src/Components/Flashcard.js
@@ -1,5 +1,13 @@
 import React, { useState } from "react";
-import { Box, Button, Card, CardContent, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  Fade,
+  Slide,
+  Typography,
+} from "@mui/material";
 import { useTheme } from "@mui/material/styles";
 import "./Flashcard.css";
 
@@ -13,6 +21,7 @@ const Flashcard = ({
   const theme = useTheme();
   const isDarkMode = theme.palette.mode === "dark";
   const [isFlipped, setIsFlipped] = useState(false);
+  const [slideIn, setSlideIn] = useState(true);
 
   const handleShowTranslation = () => {
     setIsFlipped(!isFlipped);
@@ -21,68 +30,108 @@ const Flashcard = ({
     }, 300); // Match the duration of the flip animation
   };
 
+  const handleNextCard = () => {
+    setSlideIn(false);
+    setTimeout(() => {
+      onNextCard();
+      setSlideIn(true);
+      // The timeout should be longer than the duration of the slide animation
+      // So that the first slide animation is done before the second one starts
+    }, 350);
+  };
+
   return (
-    <Card
-      className="flashcard"
+    <Slide
+      direction={slideIn ? "left" : "right"}
+      in={slideIn}
+      timeout={300}
+      mountOnEnter
+      unmountOnExit
+      easing={"ease"}
       sx={{
-        backgroundColor: isDarkMode
-          ? theme.palette.grey[900]
-          : theme.palette.grey[300],
-        transition: "transform 0.3s ease",
+        transform: slideIn ? "translateX(100vw)" : "translateX(-100vw)",
       }}
     >
-      <Box className={`flashcard-content ${isFlipped ? "flipped" : ""}`}>
-        <CardContent
-          className="flashcard-front"
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            textAlign: "center",
-          }}
-        >
-          <Typography variant="h5" component="div">
-            {frontText}
-          </Typography>
-        </CardContent>
-        <CardContent
-          className="flashcard-back"
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            textAlign: "center",
-          }}
-        >
-          <Typography variant="h5" component="div">
-            {backText}
-          </Typography>
-        </CardContent>
-      </Box>
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "space-between",
-          p: 2,
-          backgroundColor: theme.palette.action.hover,
-          borderTop: "1px solid",
-          borderColor: theme.palette.divider,
-          mt: "auto",
-        }}
-      >
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={handleShowTranslation}
-          sx={{ mr: 1, width: "80%" }}
-        >
-          {showTranslation ? "Hide Translation" : "Show Translation"}
-        </Button>
-        <Button variant="contained" color="secondary" onClick={onNextCard}>
-          Next
-        </Button>
-      </Box>
-    </Card>
+      <div>
+        <Fade in={slideIn} timeout={300} easing={"ease"}>
+          <div>
+            <Card
+              className="flashcard"
+              sx={{
+                backgroundColor: isDarkMode
+                  ? theme.palette.grey[900]
+                  : theme.palette.grey[100],
+                transition: "transform 0.3s ease",
+                "&:hover": {
+                  transform: "scale(1.1)",
+                },
+              }}
+              elevation={8}
+            >
+              <Box
+                className={`flashcard-content ${isFlipped ? "flipped" : ""}`}
+              >
+                <CardContent
+                  className="flashcard-front"
+                  sx={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    textAlign: "center",
+                  }}
+                >
+                  <Typography variant="h5" component="div">
+                    {frontText}
+                  </Typography>
+                </CardContent>
+                <CardContent
+                  className="flashcard-back"
+                  sx={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    textAlign: "center",
+                  }}
+                >
+                  <Typography variant="h5" component="div">
+                    {backText}
+                  </Typography>
+                </CardContent>
+              </Box>
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  p: 2,
+                  backgroundColor: theme.palette.action.hover,
+                  borderTop: "1px solid",
+                  borderColor: theme.palette.divider,
+                  mt: "auto",
+                }}
+              >
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={handleShowTranslation}
+                  sx={{ m: "auto", width: "45%" }}
+                  justifySelf="center"
+                >
+                  {showTranslation ? "Hide Translation" : "Show Translation"}
+                </Button>
+                <Button
+                  variant="contained"
+                  color="secondary"
+                  onClick={handleNextCard}
+                  sx={{ m: "auto", width: "45%" }}
+                >
+                  Next Card
+                </Button>
+              </Box>
+            </Card>
+          </div>
+        </Fade>
+      </div>
+    </Slide>
   );
 };
 
