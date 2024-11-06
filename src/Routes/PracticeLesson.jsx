ff2cf18f208ef403e93f86a6ac7ab3446d067f5c
Zackary Liel <47030040+zliel@users.noreply.github.com>
2024-11-06 13:02:33 -0800

    Moved the modal for reviews into its own component

--- src/Routes/PracticeLesson.jsx
+++ src/Routes/PracticeLesson.jsx
@@ -1,21 +1,14 @@
-import React, { useCallback, useEffect, useRef, useState } from "react";
-import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import React, { useRef, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import {
-  Box,
-  Modal,
-  Skeleton,
-  Typography,
-  Button,
-  IconButton,
-} from "@mui/material";
-import CloseIcon from "@mui/icons-material/Close";
+import { Box, Skeleton, Typography } from "@mui/material";
 import { useParams } from "react-router-dom";
 import { useAuth } from "../Contexts/AuthContext";
 import { useSnackbar } from "../Contexts/SnackbarContext";
 import TranslationQuestion from "../Components/TranslationQuestion";
 import "./PracticeLesson.css";
 import useLessonReview from "../hooks/useLessonReview";
+import ReviewModal from "../Components/ReviewModal";
 
 const PracticeLesson = () => {
   const { lessonId } = useParams();
@@ -110,72 +103,12 @@ const PracticeLesson = () => {
           sentence={lesson.sentences[currentSentence]}
           onNext={handleNext}
         />
-        <Modal open={modalOpen}>
-          <Box
-            sx={{
-              position: "absolute",
-              top: "50%",
-              left: "50%",
-              transform: "translate(-50%, -50%)",
-              width: 500,
-              bgcolor: "background.paper",
-              border: "2px solid #000",
-              boxShadow: 24,
-              p: 4,
-              borderRadius: 2,
-            }}
-          >
-            <IconButton
-              sx={{ position: "absolute", top: 8, right: 8 }}
-              onClick={() => setModalOpen(false)}
-            >
-              <CloseIcon />
-            </IconButton>
-            <Typography variant="h6" component="h2" textAlign={"center"}>
-              Lesson Complete!
-            </Typography>
-            <Typography sx={{ mt: 2 }}>
-              How would you rate your performance?
-            </Typography>
-            <Box
-              sx={{ mt: 2, display: "flex", justifyContent: "space-between" }}
-            >
-              <Button
-                variant="contained"
-                onClick={() => {
-                  handlePerformanceReview(0.1);
-                }}
-              >
-                Again (1)
-              </Button>
-              <Button
-                variant="contained"
-                onClick={() => {
-                  handlePerformanceReview(0.45);
-                }}
-              >
-                Hard (2)
-              </Button>
-              <Button
-                variant="contained"
-                onClick={() => {
-                  handlePerformanceReview(0.7);
-                }}
-              >
-                Good (3)
-              </Button>
-              <Button
-                variant="contained"
-                onClick={() => {
-                  handlePerformanceReview(0.9);
-                }}
-              >
-                Easy (4)
-              </Button>
-            </Box>
-          </Box>
-        </Modal>
       </Box>
+      <ReviewModal
+        open={modalOpen}
+        setOpen={setModalOpen}
+        handlePerformanceReview={handlePerformanceReview}
+      />
     </Box>
   );
 };
