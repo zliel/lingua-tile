5a96a4cf142d3a3aa13ec0479a7e80843042d88d
Zackary Liel <47030040+zliel@users.noreply.github.com>
2024-11-11 00:34:40 -0800

    Added a logo to the homepage and centered the contents of the page

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -1,12 +1,33 @@
 import React from "react";
 import { Box, Typography } from "@mui/material";
+import Logo from "../assets/LinguaTile Logo.png";
 
 function Home() {
   return (
-    <Box sx={{ height: "100%", width: "100%", margin: "auto" }}>
+    <Box
+      sx={{
+        height: "100%",
+        width: "100%",
+        margin: "auto",
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <Box
+        component={"img"}
+        sx={{
+          margin: "auto",
+          marginTop: 5,
+          display: "block",
+          marginBottom: 2,
+        }}
+        src={Logo}
+      />
       <Typography variant={"h6"} sx={{ textAlign: "center" }}>
-        Hello! Welcome to LinguaTile, a learning platform focused on the
-        Japanese language.
+        Welcome to LinguaTile, a learning platform focused on the Japanese
+        language.
       </Typography>
     </Box>
   );
