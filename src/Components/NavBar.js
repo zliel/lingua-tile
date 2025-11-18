474778bc1c8992541bf477b9e2fe9a97d5e0cc12
zliel <47030040+zliel@users.noreply.github.com>
2025-11-18 14:08:33 -0800

    Bugfix where the navbar was endlessly updating

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -40,14 +40,12 @@ function NavBar(props) {
     { name: "Translate", endpoint: "/translate" },
   ]);
 
-  const adminPages = useMemo(() => {
-    return [
-      { name: "User Table", endpoint: "/admin-users" },
-      { name: "Card Table", endpoint: "/admin-cards" },
-      { name: "Lesson Table", endpoint: "/admin-lessons" },
-      { name: "Section Table", endpoint: "/admin-sections" },
-    ];
-  }, []);
+  const adminPages = [
+    { name: "User Table", endpoint: "/admin-users" },
+    { name: "Card Table", endpoint: "/admin-cards" },
+    { name: "Lesson Table", endpoint: "/admin-lessons" },
+    { name: "Section Table", endpoint: "/admin-sections" },
+  ];
 
   const handleMenuOpen = (event) => {
     setMenuIsOpen(!menuIsOpen);
@@ -98,7 +96,7 @@ function NavBar(props) {
 
       setPages(pagesToAdd);
     }
-  }, [adminPages, authData, handleLogout, isMobile, pages]);
+  }, [authData, handleLogout, isMobile]);
 
   return (
     <AppBar
@@ -149,7 +147,7 @@ function NavBar(props) {
               width: isMobile ? "150px" : "180px",
             }}
             role="presentation"
-            onclick={handleMenuClose}
+            onClick={handleMenuClose}
             onKeyDown={handleMenuClose}
           >
             {pages.map((page) => (
