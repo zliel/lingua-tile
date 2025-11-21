e8a7c534a8f70aef3d0c2f3a6b43f69383443cdc
zliel <47030040+zliel@users.noreply.github.com>
2025-11-20 22:45:40 -0800

    Allowed the @ alias for vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,11 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   base: "/",
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@/": "/src/",
+    },
+  },
   test: {
     globals: true,
     environment: "jsdom",
