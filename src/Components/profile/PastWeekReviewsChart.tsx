3fda5e06997a4042e8405a2216fa3d3460e84869
zliel <47030040+zliel@users.noreply.github.com>
2025-11-25 15:10:45 -0800

    Added a chart that shows how many lessons were completed in the past week

---
+++ src/Components/profile/PastWeekReviewsChart.tsx
@@ -0,0 +1,44 @@
+import { useAuth } from "@/Contexts/AuthContext";
+import { Review } from "@/types/lessons"
+import { Box, Typography, useMediaQuery, useTheme } from "@mui/material";
+import { BarChart } from '@mui/x-charts/BarChart';
+import { useIsFetching } from "@tanstack/react-query";
+import dayjs from "dayjs";
+
+export const PastWeekReviewsChart = ({ reviews }: { reviews: Review[] }) => {
+  const { authData } = useAuth();
+  const isFetchingReviews = useIsFetching({ queryKey: ["reviews", authData?.token] });
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+
+  const lastWeekDays = () => {
+    return Array.from({ length: 7 }, (_, i) => dayjs().subtract(6 - i, "day").format("ddd"));
+  }
+
+  const reviewsPerDay = lastWeekDays().map(day => {
+    if (!reviews || reviews.length == 0) return 0;
+    return reviews.filter(review => dayjs(review.card_object.last_review).format("ddd") === day).length;
+  });
+
+  return (
+    <Box sx={{ my: 2 }}>
+      <Typography variant="h6" textAlign="center">
+        Lessons Reviewed This Week
+      </Typography>
+      <BarChart
+        xAxis={[{ id: "xAxis", data: lastWeekDays(), label: "Day of the Week" }]}
+        yAxis={[{ id: "yAxis", tickMinStep: 1, data: [1, 2, 3, 4, 5, 6, 7], label: "Lessons Reviewed" }]}
+        series={[
+          {
+            data: reviewsPerDay,
+            color: theme.palette.primary.main
+          },
+        ]}
+        height={300}
+        loading={isFetchingReviews > 0}
+        sx={{ minWidth: isMobile ? 100 : 600 }}
+      />
+    </Box>
+  )
+}
+
