e69bca37b9ac7c4a8abb20bad074e65ef1c4841a
zliel <47030040+zliel@users.noreply.github.com>
2025-11-24 22:03:25 -0800

    Prefetched the lessons, sections, and reviews when users go to the home page

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,9 +1,62 @@
 import { Box, Typography, useMediaQuery, useTheme } from "@mui/material";
 import Logo from "../assets/LinguaTile Logo.png";
+import { useQueryClient } from "@tanstack/react-query";
+import { useAuth } from "../Contexts/AuthContext";
+import axios from "axios";
+import { useEffect } from "react";
 
 function Home() {
+  const { authData } = useAuth();
+  const queryClient = useQueryClient();
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+
+  useEffect(() => {
+    if (authData?.token) {
+      queryClient.prefetchQuery({
+        queryKey: ["lessons", authData.token],
+        queryFn: async () => {
+          const response = await axios.get(
+            `${import.meta.env.VITE_APP_API_BASE}/api/lessons/all`,
+            {
+              headers: { Authorization: `Bearer ${authData.token}` },
+            },
+          );
+          return response.data;
+        },
+        staleTime: 5 * 60 * 1000, // 5 minutes
+      });
+
+      queryClient.prefetchQuery({
+        queryKey: ["sections", authData.token],
+        queryFn: async () => {
+          const response = await axios.get(
+            `${import.meta.env.VITE_APP_API_BASE}/api/sections/all`,
+            {
+              headers: { Authorization: `Bearer ${authData.token}` },
+            },
+          );
+          return response.data;
+        },
+        staleTime: 5 * 60 * 1000, // 5 minutes
+      });
+
+      queryClient.prefetchQuery({
+        queryKey: ["reviews", authData.token],
+        queryFn: async () => {
+          const response = await axios.get(
+            `${import.meta.env.VITE_APP_API_BASE}/api/lessons/reviews/`,
+            {
+              headers: { Authorization: `Bearer ${authData.token}` },
+            },
+          );
+          return response.data;
+        },
+        staleTime: 5 * 60 * 1000, // 5 minutes
+      });
+    }
+  }, [authData, queryClient]);
+
   return (
     <Box
       sx={{
