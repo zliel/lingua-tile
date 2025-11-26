de5f1102a14aa994e97543bf1d86d59cc74774c0
zliel <47030040+zliel@users.noreply.github.com>
2025-11-25 16:17:08 -0800

    Added a chart that shows the FSRS difficulty of each user's learned lessons

---
+++ src/Components/profile/LessonDifficultyChart.tsx
@@ -0,0 +1,70 @@
+import { useAuth } from "@/Contexts/AuthContext";
+import { Review } from "@/types/lessons"
+import { Box, Typography, useMediaQuery, useTheme } from "@mui/material";
+import { BarChart } from '@mui/x-charts/BarChart';
+import { useIsFetching } from "@tanstack/react-query";
+
+export const LessonDifficultyChart = ({ reviews }: { reviews: Review[] }) => {
+  const { authData } = useAuth();
+  const isFetchingReviews = useIsFetching({ queryKey: ["reviews", authData?.token] });
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+
+  const difficultyCounts = () => {
+    const counts = Array(10).fill(0);
+
+    reviews?.forEach(review => {
+      const difficulty = review.card_object.difficulty ?? 0; // Will be something like 7.13013
+      const index = Math.min(Math.floor(difficulty / 10), 9); // Map to index 0-9
+      counts[index]++;
+    });
+
+    return counts;
+  }
+
+  return (
+    <>
+      <Box sx={{ my: 2 }}>
+        <Typography variant="h6" textAlign="center">
+          Lesson Difficulty
+        </Typography>
+        <Typography variant="subtitle1" textAlign="center">
+          Used by FSRS to Help With Scheduling
+        </Typography>
+        <BarChart
+          xAxis={
+            [{
+              id: "xAxis",
+              label: "Lesson Difficulty (%)",
+              data: [0, 10, 20, 30, 40, 50, 60, 70, 80, 90],
+              valueFormatter: (value: number) => `${value}-${value + 10}%`,
+              colorMap: {
+                type: 'continuous',
+                min: 0,
+                max: 90,
+                color: [theme.palette.primary.main, theme.palette.secondary.main]
+              }
+            }]
+          }
+          series={[
+            {
+              data: difficultyCounts(),
+            }
+          ]}
+          yAxis={
+            [{
+              id: "yAxis",
+              label: "Number of Lessons",
+              tickMinStep: 1,
+              width: 35
+            }]
+          }
+          loading={isFetchingReviews > 0}
+          height={300}
+          sx={{ minWidth: isMobile ? 150 : 700 }}
+        />
+      </Box>
+    </>
+  );
+}
+
