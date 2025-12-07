142f466cef7fe22a3f7431fd474099c173784088
zliel <47030040+zliel@users.noreply.github.com>
2025-12-06 16:59:25 -0800

    Added a swiping in and out animation to the flashcard list

--- src/Components/skeletons/FlashcardListSkeleton.tsx
+++ src/Components/skeletons/FlashcardListSkeleton.tsx
@@ -16,13 +16,64 @@ const FlashcardListSkeleton = () => {
         px: isMobile ? 1 : 0,
       }}
     >
-      <Skeleton
-        variant="rectangular"
-        width={isMobile ? "100%" : 400}
-        height={isMobile ? 400 : 500}
-        sx={{ borderRadius: 4, mb: 3 }}
-      />
+      <Box
+        sx={{
+          position: "relative",
+          width: isMobile ? "90%" : 400,
+          height: isMobile ? 400 : 500,
+          mb: 2,
+        }}
+      >
+        {/* Static Background Card Stack */}
+        <>
+          <Skeleton
+            variant="rectangular"
+            width={isMobile ? "100%" : 400}
+            height={isMobile ? 400 : 500}
+            animation={false}
+            sx={{
+              position: "absolute",
+              top: 0,
+              left: -12,
+              borderRadius: 4,
+              opacity: 0.2,
+              transform: "rotate(2deg)",
+              zIndex: 0,
+            }}
+          />
+          <Skeleton
+            variant="rectangular"
+            width={isMobile ? "100%" : 400}
+            height={isMobile ? 400 : 500}
+            animation={false}
+            sx={{
+              position: "absolute",
+              top: 0,
+              left: -6, // Slight offset
+              borderRadius: 4,
+              opacity: 0.2,
+              transform: "rotate(-1deg)", // Static rotation
+              zIndex: 0,
+            }}
+          />
+        </>
 
+        {/* Main Card with Swipe Animation */}
+        <Skeleton
+          variant="rectangular"
+          width={isMobile ? "100%" : 400}
+          height={isMobile ? 400 : 500}
+          sx={{
+            borderRadius: 4,
+            mb: 3,
+            position: "relative",
+            zIndex: 1,
+            animation: "swipe-out-in 1.5s infinite ease-in-out",
+          }}
+        />
+      </Box>
+
+      {/* Progress Bar Skeleton */}
       <Box
         sx={{
           width: "100%",
