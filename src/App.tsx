6143f78d96077601a72ed3a53964712f17fc187a
zliel <47030040+zliel@users.noreply.github.com>
2026-01-02 13:04:25 -0800

    Moved theme definitions into their own file

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import "./App.css";
 import {
-  ColorSystemOptions,
-  createTheme,
   CssBaseline,
   ThemeProvider,
   useColorScheme,
@@ -11,38 +9,7 @@ import { SnackbarProvider } from "./Contexts/SnackbarContext";
 import { AuthProvider } from "./Contexts/AuthContext";
 import { OfflineProvider } from "./Contexts/OfflineContext";
 import { AppRoutes } from "./Routes/AppRoutes";
-
-const purpleBase = "#8c00cc";
-const greenBase = "#00c800";
-
-const palette: ColorSystemOptions = {
-  palette: {
-    primary: {
-      main: purpleBase,
-    },
-    secondary: {
-      main: greenBase,
-      contrastText: "#fff",
-    },
-    grammar: {
-      main: "#0288d1",
-      light: "#5eb8ff",
-      dark: "#005b9f",
-      contrastText: "#fff",
-    },
-  },
-};
-
-const theme = createTheme({
-  colorSchemes: {
-    light: {
-      ...palette,
-    },
-    dark: {
-      ...palette,
-    },
-  },
-});
+import { theme } from "./theme"
 
 function App() {
   const { mode, setMode } = useColorScheme();
