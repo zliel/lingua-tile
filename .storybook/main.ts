1682be99cdd9fbec4d7482ec2606d643d95d5531
zliel <47030040+zliel@users.noreply.github.com>
2026-01-05 17:56:20 -0800

    Reverting previous change

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,6 +1,5 @@
 
 import type { StorybookConfig } from "@storybook/react-vite";
-import path from "path";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
@@ -12,22 +11,6 @@ const config: StorybookConfig = {
     "@storybook/addon-docs",
     "@storybook/addon-themes",
   ],
-  async viteFinal(config) {
-    // Explicitly set up alias for Storybook's Vite builder
-    config.resolve = config.resolve || {};
-    config.resolve.alias = {
-      ...(config.resolve.alias || {}),
-      '@': path.resolve(__dirname, '../src'),
-    };
-
-    // If you use vite-tsconfig-paths, ensure it's included here too
-    if (config.plugins) {
-      const tsconfigPaths = require('vite-tsconfig-paths').default;
-      config.plugins.push(tsconfigPaths());
-    }
-
-    return config;
-  },
   framework: "@storybook/react-vite",
 };
 export default config;
